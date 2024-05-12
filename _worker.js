918ef61ca6988c8d1d4a98b89ddf0cb815b81999
Alizhangs <91523641+Alizhangs@users.noreply.github.com>
2024-05-12 18:25:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // src/worker.js
 import { connect } from "cloudflare:sockets";
 let password = 'moist';
-let proxyIP = '18.141.185.144';
+let proxyIP = '172.93.188.157';
 
 let addresses = [
 	//当sub为空时启用本地优选域名/优选IP，若不带端口号 TLS默认端口为443，#号后为备注别名
